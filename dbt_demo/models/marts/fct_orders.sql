5de432129c817aebc269b4345f39e7fe15c3cca1
sparkbrains-DE <sukhpreet.singh@sparkbrains.in>
2025-12-16 06:08:23 +0000

    2 MODELS ADDED

---
+++ dbt_demo/models/marts/fct_orders.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='table') }}
+
+with orders as (
+    select * from {{ ref('stg_tpch_orders') }}
+),
+
+customers as (
+    select
+        c_custkey     as customer_id,
+        c_name        as customer_name,
+        c_nationkey   as nation_id,
+        c_acctbal     as account_balance,
+        c_mktsegment  as market_segment
+    TEMP_DB.TEMP_SCHEMA.TEMP_CUSTOMER
+)
+
+select
+    o.order_id,
+    o.order_date,
+    o.order_status,
+    o.order_priority,
+    o.customer_id,
+    c.customer_name,
+    c.market_segment,
+    o.total_price
+from orders o
+left join customers c
+    on o.customer_id = c.customer_id
---
+++ dbt_demo/models/staging/stg_tpch_orders.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+select
+    o_orderkey            as order_id,
+    o_custkey             as customer_id,
+    o_orderstatus         as order_status,
+    o_totalprice          as total_price,
+    o_orderdate           as order_date,
+    o_orderpriority       as order_priority,
+    o_clerk               as clerk,
+    o_shippriority        as ship_priority,
+    o_comment             as order_comment
+from TEMP_DB.TEMP_SCHEMA.TEMP_ORDERS
